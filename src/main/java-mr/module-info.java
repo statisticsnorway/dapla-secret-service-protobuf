971265e728319b25020a719437e0f8e7f38ba9b2
Øyvind Strømmen <insula.ventus@gmail.com>
2020-01-22 12:54:44 +0100

    Define the protobuf api

---
+++ src/main/java-mr/module-info.java
@@ -0,0 +1,10 @@
+module no.ssb.dapla.secret.service.protobuf {
+    requires com.google.common;
+    requires io.grpc;
+    requires grpc.stub;
+    requires com.google.protobuf;
+    requires grpc.protobuf;
+    requires java.annotation;
+
+    exports no.ssb.dapla.secret.service.protobuf;
+}
